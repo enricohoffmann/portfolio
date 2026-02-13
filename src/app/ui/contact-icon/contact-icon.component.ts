1c45f2532caf1b7b9a3cfa9822528a0e8498faf6
Enrico Hoffmann <enricohoffmann@web.de>
2026-02-13 20:41:11 +0100

    Refactors lateral UI for improved layout and icon hover
    Switches lateral panel to grid layout for better alignment.
    Simplifies contact icon logic and enables CSS-driven hover effects.
    Enhances visual cues with dot indicator and adjusts styling for consistency.

--- src/app/ui/contact-icon/contact-icon.component.ts
+++ src/app/ui/contact-icon/contact-icon.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contact-icon',
@@ -7,45 +7,8 @@ import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular
   templateUrl: './contact-icon.component.html',
   styleUrl: './contact-icon.component.scss'
 })
-export class ContactIconComponent implements AfterViewInit {
+export class ContactIconComponent {
   @Input() iconName?: string;
-  @ViewChild('iconDefault', { static: true }) iconDefaultElement!: ElementRef<HTMLImageElement>;
-  @ViewChild('iconHover', { static: true }) iconHoverElement!: ElementRef<HTMLImageElement>;
 
-  ngAfterViewInit(): void {
-    this.setupIcon();
-  }
-
-  onMouseEnter() {
-    const iconDefault = this.iconDefaultElement.nativeElement;
-    const iconHover = this.iconHoverElement.nativeElement;
-    if (iconDefault || !iconHover) { return; }
-  }
-
-  onMouseLeave() {
-    /* this.setIcon('Colored'); */
-  }
-
-  setupIcon(): void {
-    const iconDefault = this.iconDefaultElement.nativeElement;
-    const iconHover = this.iconHoverElement.nativeElement;
-    if (!iconDefault || !iconHover || !this.iconName) { return; }
-
-    this.setIconIntoImg(iconDefault, 'default');
-    this.setIconIntoImg(iconHover, 'hover');
-    
-  }
-
-  setIconIntoImg(icon: HTMLImageElement, type: 'default' | 'hover'):void{
-    const path = type === "default" ? `/assets/icon/${this.iconName}Colored.svg` : `/assets/icon/${this.iconName}White.svg`;
-    fetch(path).then(response => {
-      if (response.ok) {
-        icon.src = path;
-        icon.classList.add(type === 'default' ? 'showColored' : 'showWhite')
-      }
-    }).catch(() => {
-      return;
-    });
-  }
 
 }
