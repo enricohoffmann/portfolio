aaeaa4c9dd924aab4474975db1f87e3ec891be79
Enrico Hoffmann <enricohoffmann@web.de>
2026-03-05 20:12:09 +0100

    Improves contact input validation and overlay UX
    Refactors input fields to use Angular's validation more effectively,
    adds dynamic error messaging, and replaces native placeholders with
    custom overlays for better feedback and accessibility. Enhances
    visual distinction between valid and invalid states, especially for
    email input, and improves focus handling for a more intuitive user
    experience.

--- src/app/features/contact/contact-item/contact-item.component.ts
+++ src/app/features/contact/contact-item/contact-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ContactData } from '../../../interfaces/contact.interface';
-import { ControlContainer, FormsModule, NgForm } from '@angular/forms';
+import { ControlContainer, FormsModule, NgForm, NgModel } from '@angular/forms';
 @Component({
   selector: 'app-contact-item',
   standalone: true,
@@ -14,10 +14,13 @@ import { ControlContainer, FormsModule, NgForm } from '@angular/forms';
 export class ContactItemComponent {
   @Input({required: true}) contactData!: ContactData;
   @Input({required: true}) contactFieldIndex!: string;
-  inputValue?: string;
+  readonly emailPattern = '[a-zA-Z0-9._%+\\-]+@[a-zA-Z0-9.\\-]+\\.[a-zA-Z]{2,}';
   isChanged: boolean = false;
   placeHolderText: string = "";
   hasError: boolean = false;
+  hasFocus: boolean = false;
+  errorMessage: string = "";
+
 
   ngOnInit(){
     if(this.contactData){
@@ -34,20 +37,52 @@ export class ContactItemComponent {
     
   }
 
-  onInput(): void{
+  onInput(field: NgModel): void{
     this.isChanged = true;
-    this.hasError = false;
+    this.checkFildValidation(field);
   }
 
-  onBlur(){
+  onBlur(field: NgModel): void{
+    this.isChanged = false;
+    this.checkFildValidation(field);
+  }
 
-    if(this.contactData.contactValue){ 
-      this.hasError = false;
-    }else{
-      this.placeHolderText = this.contactData?.errorText ?? "";
-      this.hasError = true;
+  checkFildValidation(field: NgModel): void{
+    const showErrors = !!field.touched;
+    this.hasError = showErrors && !!field.invalid;
+
+    if(!showErrors){
+      this.errorMessage = "";
+      return;
     }
-    
+
+    if(field.errors?.['required']){
+      this.errorMessage = this.contactData.errorText;
+      return;
+    }
+
+    if(field.errors?.['pattern']){
+      this.errorMessage = 'The email address is invalid.';
+      return;
+    }
+
+    if(field.errors?.['minlength']){
+      this.errorMessage = `The input is too short. (minimum ${field.errors['minlength'].requiredLength} characters)`;
+      return;
+    }
+
+    if(field.valid){
+      this.errorMessage = "";
+    }
+
+  }
+
+  hasText(field: NgModel): boolean {
+    if (typeof field.value === 'string') {
+      return field.value.trim().length > 0;
+    }
+
+    return !!field.value;
   }
 
 }
