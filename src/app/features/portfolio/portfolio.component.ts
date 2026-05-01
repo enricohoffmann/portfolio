e0814fb0cc191d7c42dd2f19928fb8f66358a035
Enrico Hoffmann <enricohoffmann@web.de>
2026-05-01 08:37:12 +0200

    Enhances portfolio docs and refines header styling
    Adds detailed JSDoc comments to improve code clarity and maintainability for the portfolio feature. Refines header component styles for better spacing and mobile responsiveness. Improves overall developer experience and UI consistency.

--- src/app/features/portfolio/portfolio.component.ts
+++ src/app/features/portfolio/portfolio.component.ts
@@ -13,6 +13,14 @@ import { LanguageService } from '../../services/language.service';
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
+
+/**
+ * @description The PortfolioComponent is responsible for displaying the portfolio section of the application. It retrieves project data from the PortfolioDataService and manages the selection of projects through event relaying. 
+ * The component also handles language-specific data for the portfolio section using the LanguageService.
+ * @namespace PortfolioComponent
+ * @memberof App.Features.Portfolio
+ * @property {ProjectEntry[]} portfolioPojects - An array of project entries to be displayed in the portfolio.
+ */
 export class PortfolioComponent {
 
   portfolioPojects: ProjectEntry[] = [];
@@ -23,8 +31,21 @@ export class PortfolioComponent {
     this.portfolioPojects = this.portfolioData.getProjects();
   }
   
+  /**
+   * @description Observable that provides the portfolio section data based on the selected language.
+   * @type {Observable<ProjectSectionData>}
+   * @returns An observable that emits the portfolio section data corresponding to the current language selection.
+   * @memberof PortfolioComponent
+   */
   portfolioSectionData$: Observable<ProjectSectionData> = this.languageService.selectByLanguage(this.portfolioData.getProjectSectionDataDe(), this.portfolioData.getProjectSectionDataEn());
 
+  /**
+   * @description Relays the selected project ID to the parent component or other interested components through the selectPortfolioProjectByIdRelay event emitter.
+   * This method is called when a project is selected from the portfolio menu, allowing other components to react to the selection and display relevant project details.
+   * @param projectId 
+   * @memberof PortfolioComponent
+   * @returns void
+   */
   onProjectIdRelaying(projectId: string): void{
     this.selectPortfolioProjectByIdRelay.emit(projectId);
   }
